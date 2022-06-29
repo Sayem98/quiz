5da91a0248d6ea8c6b85f10bbfe4bd4f3f05d5d9
Sayem98 <sayemabedin.bd@gmail.com>
2022-06-29 20:39:11 +0600

    provider of auth is used.

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
-
+//problem with environment variable.
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_KEY,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBx4pMSiKflxWTY5j6rOjWtGZY-KlXXa_Y",
+  authDomain: "quiz-dev-b7f2b.firebaseapp.com",
+  projectId: "quiz-dev-b7f2b",
+  storageBucket: "quiz-dev-b7f2b.appspot.com",
+  messagingSenderId: "104859974249",
+  appId: "1:104859974249:web:207f134404e038614c00f8",
 });
 
 export default app;
