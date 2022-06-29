7e3ceaf1a387abbc19c1c727df4fa38114d7d0d4
Sayem98 <sayemabedin.bd@gmail.com>
2022-06-29 20:17:51 +0600

    Authentication context is created

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "../styles/App.css";
+import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,15 +10,17 @@ import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </AuthProvider>
   );
 }
 
